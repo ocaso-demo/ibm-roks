614bbaac8f0bc5f6a3e86741db7680796d2fedd3
Jesus Almaraz <jesus.mah@gmail.com>
2022-10-07 14:57:15 +0200

    test

--- main.tf
+++ main.tf
@@ -58,30 +58,30 @@ data "ibm_resource_instance" "cos_instance" {
   service           = "cloud-object-storage"
 }
 
-# resource "ibm_container_vpc_cluster" "cluster" {
-#   name                            = local.CLUSTERNAME
-#   vpc_id                          = data.ibm_is_vpc.vpc.id
-#   kube_version                    = local.KUBEVERSION
-#   flavor                          = local.NODEFLAVOR
-#   worker_count                    = local.NUMNODES
-#   resource_group_id               = data.ibm_resource_group.group.id
-#   # entitlement                     = "cloud_pak"
-#   disable_public_service_endpoint = false
-#   cos_instance_crn                = data.ibm_resource_instance.cos_instance.crn
-#   force_delete_storage            = true
-#   zones {
-#     name                          = "eu-de-1"
-#     subnet_id                     = data.ibm_is_subnet.subnet1.id
-#   }
-#   zones {
-#     name                          = "eu-de-2"
-#     subnet_id                     = data.ibm_is_subnet.subnet2.id
-#   }
-#   zones {
-#     name                          = "eu-de-3"
-#     subnet_id                     = data.ibm_is_subnet.subnet3.id
-#   }
-# }
+resource "ibm_container_vpc_cluster" "cluster" {
+  name                            = local.CLUSTERNAME
+  vpc_id                          = data.ibm_is_vpc.vpc.id
+  kube_version                    = local.KUBEVERSION
+  flavor                          = local.NODEFLAVOR
+  worker_count                    = local.NUMNODES
+  resource_group_id               = data.ibm_resource_group.group.id
+  # entitlement                     = "cloud_pak"
+  disable_public_service_endpoint = false
+  cos_instance_crn                = data.ibm_resource_instance.cos_instance.crn
+  force_delete_storage            = true
+  zones {
+    name                          = "eu-de-1"
+    subnet_id                     = data.ibm_is_subnet.subnet1.id
+  }
+  zones {
+    name                          = "eu-de-2"
+    subnet_id                     = data.ibm_is_subnet.subnet2.id
+  }
+  zones {
+    name                          = "eu-de-3"
+    subnet_id                     = data.ibm_is_subnet.subnet3.id
+  }
+}
 
 # # Download kubeconfig for the cluster into home dir
 # resource "null_resource" "kubeconfig" {
