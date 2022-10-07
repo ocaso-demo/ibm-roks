d636f4bf511cdc1bbc56a36bf092b2f052ee9a24
Jesus Almaraz <jesus.mah@gmail.com>
2022-10-07 13:19:15 +0200

    test

--- main.tf
+++ main.tf
@@ -58,30 +58,30 @@ data "ibm_resource_instance" "cos_instance" {
   service           = "cloud-object-storage"
 }
 
-resource "ibm_container_vpc_cluster" "cluster" {
-  name                            = local.CLUSTERNAME
-  vpc_id                          = data.ibm_is_vpc.vpc.id
-  kube_version                    = local.KUBEVERSION
-  flavor                          = local.NODEFLAVOR
-  worker_count                    = local.NUMNODES
-  resource_group_id               = data.ibm_resource_group.group.id
-  # entitlement                     = "cloud_pak"
-  disable_public_service_endpoint = false
-  cos_instance_crn                = data.ibm_resource_instance.cos_instance.crn
-  force_delete_storage            = true
-  zones {
-    name                          = "eu-de-1"
-    subnet_id                     = data.ibm_is_subnet.subnet1.id
-  }
-  zones {
-    name                          = "eu-de-2"
-    subnet_id                     = data.ibm_is_subnet.subnet2.id
-  }
-  zones {
-    name                          = "eu-de-3"
-    subnet_id                     = data.ibm_is_subnet.subnet3.id
-  }
-}
+# resource "ibm_container_vpc_cluster" "cluster" {
+#   name                            = local.CLUSTERNAME
+#   vpc_id                          = data.ibm_is_vpc.vpc.id
+#   kube_version                    = local.KUBEVERSION
+#   flavor                          = local.NODEFLAVOR
+#   worker_count                    = local.NUMNODES
+#   resource_group_id               = data.ibm_resource_group.group.id
+#   # entitlement                     = "cloud_pak"
+#   disable_public_service_endpoint = false
+#   cos_instance_crn                = data.ibm_resource_instance.cos_instance.crn
+#   force_delete_storage            = true
+#   zones {
+#     name                          = "eu-de-1"
+#     subnet_id                     = data.ibm_is_subnet.subnet1.id
+#   }
+#   zones {
+#     name                          = "eu-de-2"
+#     subnet_id                     = data.ibm_is_subnet.subnet2.id
+#   }
+#   zones {
+#     name                          = "eu-de-3"
+#     subnet_id                     = data.ibm_is_subnet.subnet3.id
+#   }
+# }
 
 
 # Run boostrap.sh
@@ -95,44 +95,44 @@ resource "null_resource" "test" {
   }
 }
 
-# Download kubeconfig for the cluster into home dir
-resource "null_resource" "kubeconfig" {
+# # Download kubeconfig for the cluster into home dir
+# resource "null_resource" "kubeconfig" {
 
-  provisioner "local-exec" {
-    command = "./scripts/get-kubeconfig.sh || true"
-    environment = {
-      HOME        = "${abspath(path.module)}/home"
-      API_KEY     = local.API_KEY
-      CLUSTERNAME = resource.ibm_container_vpc_cluster.cluster.name
-    }
-  }
+#   provisioner "local-exec" {
+#     command = "./scripts/get-kubeconfig.sh || true"
+#     environment = {
+#       HOME        = "${abspath(path.module)}/home"
+#       API_KEY     = local.API_KEY
+#       CLUSTERNAME = resource.ibm_container_vpc_cluster.cluster.name
+#     }
+#   }
 
-  depends_on = [
-    ibm_container_vpc_cluster.cluster
-  ]
-}
+#   depends_on = [
+#     ibm_container_vpc_cluster.cluster
+#   ]
+# }
 
-# Run boostrap.sh
-resource "null_resource" "argo_bootstrap" {
+# # Run boostrap.sh
+# resource "null_resource" "argo_bootstrap" {
 
-  provisioner "local-exec" {
-    command = "chmod +x ./scripts/bootstrap.sh && ./scripts/bootstrap.sh || true"
-    environment = {
-      HOME = "${abspath(path.module)}/home"
-    }
-  }
+#   provisioner "local-exec" {
+#     command = "chmod +x ./scripts/bootstrap.sh && ./scripts/bootstrap.sh || true"
+#     environment = {
+#       HOME = "${abspath(path.module)}/home"
+#     }
+#   }
 
-  depends_on = [
-    null_resource.kubeconfig
-  ]
-}
+#   depends_on = [
+#     null_resource.kubeconfig
+#   ]
+# }
 
-data "external" "argo_info" {
-  program = ["bash", "${path.module}/scripts/get-argo-info.sh"]
-  query = {
-    kubeconfig = "${abspath(path.module)}/home/.kube/config"
-  }
-  depends_on = [
-    null_resource.argo_bootstrap
-  ]
-}
+# data "external" "argo_info" {
+#   program = ["bash", "${path.module}/scripts/get-argo-info.sh"]
+#   query = {
+#     kubeconfig = "${abspath(path.module)}/home/.kube/config"
+#   }
+#   depends_on = [
+#     null_resource.argo_bootstrap
+#   ]
+# }
